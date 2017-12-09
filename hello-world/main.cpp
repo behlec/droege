130aa56cc325913a9f8615dfcdfe5c185a3d8cf8
Christoph Behle <behlec@gmail.com>
2017-12-09 13:46:10 +0100

    Add clang

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 
-int main() { std::cout << "Hello world\n"; }
+int main() {
+  std::cout << "Hello world\n";
+#ifdef __GNUC__
+  std::cout << "g++\n";
+#endif
+#ifdef __clang__
+  std::cout << "clang++\n";
+#endif
+}
